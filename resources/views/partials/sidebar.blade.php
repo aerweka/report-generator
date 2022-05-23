95f2b9f1a002698ca968ba4565def0b68446ee47
Alerio Reyhan <alerioreyhan127@gmail.com>
2022-05-23 14:40:06 +0700

    setup layout, laporan views, plugins

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,38 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+	<!-- Brand Logo -->
+	<a href="/laporan" class="brand-link">
+		<img src="{{ asset('logo-pdam.png') }}" alt="PDAM Surabaya Logo" class="brand-image img-circle elevation-1">
+		<span class="brand-text font-weight-light">PDAM Surabaya</span>
+	</a>
+
+	<!-- Sidebar -->
+	<div class="sidebar">
+
+		<!-- Sidebar Menu -->
+		<nav class="mt-2">
+			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+				<!-- Add icons to the links using the .nav-icon class
+													with font-awesome or any other icon font library -->
+				<li class="nav-item">
+					<a href="/laporan" class="nav-link">
+						<i class="nav-icon fas fa-book"></i>
+						<p>
+							Laporan
+						</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="../widgets.html" class="nav-link">
+						<i class="nav-icon fas fa-th"></i>
+						<p>
+							Widgets
+							<span class="right badge badge-danger">New</span>
+						</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- /.sidebar-menu -->
+	</div>
+	<!-- /.sidebar -->
+</aside>
